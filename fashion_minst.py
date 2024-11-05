53e3eab243ff0b6fe3199472b115f2ceb7e59a76
Jan Kardaszewicz <jjkardaszewicz@gmail.com>
2024-11-05 10:47:36 +0100

    add wandb

--- fashion_minst.py
+++ fashion_minst.py
@@ -4,6 +4,12 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import numpy as np
+import wandb
+
+wandb.init(
+    project="FashionMNIST",
+    config={"epochs": 5, "batch_size": 128, "learning_rate": 0.001},
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -65,22 +71,29 @@ for epoch in range(epochs):
         optimizer.step()
 
         running_loss += loss.item()
+    avg_loss = running_loss / len(train_loader)
 
-    print(f"Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(train_loader):.4f}")
+    print(f"Epoch [{epoch + 1}/{epochs}], Loss: {avg_loss:.4f}")
+    wandb.log({"loss": avg_loss})
 
 
 model.eval()
 all_preds = []
 all_labels = []
+val_loss = 0.0
 
 with torch.no_grad():
     for images, labels in test_loader:
         images, labels = images.to(device), labels.to(device)
         outputs = model(images)
+        loss = criterion(outputs, labels)
+        val_loss += loss.item()
         _, predicted = torch.max(outputs.data, 1)
         all_preds.extend(predicted.cpu().numpy())
         all_labels.extend(labels.cpu().numpy())
 
+avg_val_loss = val_loss / len(test_loader)
+wandb.log({"validation_loss": avg_val_loss})
 confusion_matrix = np.zeros((10, 10), dtype=int)
 
 for true, pred in zip(all_labels, all_preds):
@@ -90,3 +103,10 @@ accuracies = confusion_matrix.diagonal() / confusion_matrix.sum(axis=1)
 
 for i, acc in enumerate(accuracies):
     print(f"Dokładność klasy {i}: {acc:.2f}")
+    wandb.log({f"class_{i}_accuracy": acc})
+
+artifact = wandb.Artifact("fashion_mnist_model", type="code")
+artifact.add_file("/home/jk/Documents/studia/sieci/lab3/mldevops_exercise/fashion_minst.py")
+wandb.log_artifact(artifact)
+
+wandb.finish()
